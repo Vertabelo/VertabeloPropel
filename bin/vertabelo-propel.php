e03e8e24eb7cc46cc354f28b6effea5c69e6b8c2
Agnieszka Kozubek-Krycuń <akozubek@e-point.pl>
2014-11-26 11:38:57 +0100

    Fixed autoloading for installation via Composer

--- bin/vertabelo-propel.php
+++ bin/vertabelo-propel.php
@@ -19,7 +19,11 @@
  *
  */
 
-require_once __DIR__.'/../vendor/autoload.php';
+if (file_exists(__DIR__.'/../vendor/autoload.php')) {
+	require_once __DIR__.'/../vendor/autoload.php';
+} elseif (file_exists(__DIR__.'/../../../autoload.php'))  {
+ 	require_once(__DIR__.'/../../../autoload.php');
+}
 
 use Vertabelo\VertabeloPropel\VertabeloApplication;
 use Vertabelo\VertabeloPropel\VertabeloPropelCommand;
